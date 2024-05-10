94bc60d03b6916540f7ab5074dcc2a6af7d2ed09
Ronnie Gentile <rgentile09@yahoo.com>
2024-05-09 20:28:52 -0500

    MVC studio complete

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,30 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+    if(user.getPassword().equals(verify)) {
+        return "user/index";
+    } else {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("error", "passwords don't match!");
+        return "user/add";
+    }
+    }
+}
